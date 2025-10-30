5393c580a45fff3d76a7c665f3d5bcc4e3eb40c5
박보성 <bagboseong@bagboseong-ui-MacBookPro.local>
2025-10-30 13:35:16 +0900

    connect mysql

---
+++ demo/src/main/java/SprSecu/demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package SprSecu.demo.repository;
+
+import SprSecu.demo.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+
+}
