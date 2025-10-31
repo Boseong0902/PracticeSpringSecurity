1b1fdc62d3db7588f26b6fb3252803215e286580
박보성 <bagboseong@bagboseong-ui-MacBookPro.local>
2025-10-31 12:40:02 +0900

    UsernamePasswordFilter구현

---
+++ demo/src/main/java/SprSecu/demo/jwt/LoginFilter.java
@@ -0,0 +1,39 @@
+package SprSecu.demo.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationmanager;
+
+    public LoginFilter(AuthenticationManager authenticationmanager){
+        this.authenticationmanager = authenticationmanager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response){
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        return authenticationmanager.authenticate(token);
+    }
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+
+    }
+
+}
